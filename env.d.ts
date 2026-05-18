87b7f9017a65599d6378a1f0d58fdf10d5ca5e2e
Mesho254 <meshakotieno343@gmail.com>
2026-05-18 16:31:49 +0300

    fixed aos issue

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,23 @@
 /// <reference types="vite/client" />
+
+declare module 'aos' {
+  interface AOSOptions {
+    duration?: number
+    delay?: number
+    easing?: string
+    once?: boolean
+    mirror?: boolean
+    anchorPlacement?: string
+    offset?: number
+    disable?: boolean | string
+  }
+
+  interface AOS {
+    init(options?: AOSOptions): void
+    refresh(): void
+    refreshHard(): void
+  }
+
+  const aos: AOS
+  export default aos
+}
